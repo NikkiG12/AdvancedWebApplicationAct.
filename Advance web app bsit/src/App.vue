83b68a83e47b6d9187f2f5f1a2c7a5a221256547
Nike Shian G. Guray <nikkiguray12@gmail.com>
2023-10-03 22:51:03 +0800

    Activty3

--- Advance web app bsit/src/App.vue
+++ Advance web app bsit/src/App.vue
@@ -1,3 +1,19 @@
-
-<template></template>
+<script setup>
+import { ref } from 'vue';
+const input = ref('')
+</script>
+<template>
+    <!-- two-way binding -->
+    <select v-model="input">
+        <option value="NS">NS</option>
+        <option value="AC">AC</option>
+        <option value="MM">MM</option>
+        <option value="R">R</option>
+    </select> 
+    <h1 v-if="input == 'NS'" style="font-family: 'Pacifico', cursive; font-size: 50px;">Nike Shian</h1>
+    <p v-else-if="input == 'AC'" style="font-family: 'Pacifico', cursive; font-size: 50px;">Angelo Cris</p>
+    <p v-else-if="input == 'MM'" style="font-family: 'Pacifico', cursive; font-size: 50px;">Mary Mayumi</p>
+    <p v-else-if="input == 'R'" style="font-family: 'Pacifico', cursive; font-size: 50px;">Riza</p>
+    <p v-else style="font-family: 'Pacifico', cursive; font-size: 50px;">Others</p>
+</template>
 
